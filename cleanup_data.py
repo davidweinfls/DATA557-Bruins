36d26a7c01bbde619e12ec03a106c2bcc80247d8
Naman Sharma <namansh.is@gmail.com>
2019-03-02 14:00:59 -0800

    adding main dataset

--- cleanup_data.py
+++ cleanup_data.py
@@ -1,45 +1,49 @@
 from pandas import read_csv, to_numeric, io, to_datetime
 from pandas.io.json import json_normalize
-import sys, os, traceback
+import sys
+import os
+import traceback
 
 if len(sys.argv) < 2:
-    print("Please specify your input file")
-    sys.exit(1)
+  print("Please specify your input file")
+  sys.exit(1)
 
 input_file = sys.argv[1]
 
 if not os.path.isfile(input_file):
-    traceback.print_exc(file=sys.stdout)
-    sys.exit(1)
+  traceback.print_exc(file=sys.stdout)
+  sys.exit(1)
 
 ListingFile = read_csv(input_file)
 
+
 def CleanAndSplit(inputVal,
-                  SplitByChar = ',',
-                  enclosureChar = '{|}|[|]',
-                  stripChars = '"',
-                  alsoRemoveSingleInvComma = False):
-    inputVal = inputVal.strip()
-    inputVal = inputVal.strip(enclosureChar)
-    inputVal = inputVal.split(SplitByChar)
-    if alsoRemoveSingleInvComma == True:
-        inputVal = [item.strip().strip(stripChars).strip("'").strip() for item in inputVal]
-    else:
-        inputVal = [item.strip().strip(stripChars).strip() for item in inputVal]
+                  SplitByChar=',',
+                  enclosureChar='{|}|[|]',
+                  stripChars='"',
+                  alsoRemoveSingleInvComma=False):
+  inputVal = inputVal.strip()
+  inputVal = inputVal.strip(enclosureChar)
+  inputVal = inputVal.split(SplitByChar)
+  if alsoRemoveSingleInvComma == True:
+    inputVal = [item.strip().strip(stripChars).strip("'").strip() for item in inputVal]
+  else:
+    inputVal = [item.strip().strip(stripChars).strip() for item in inputVal]
 
-    OutputDict = {}
-    for item in inputVal:
-        OutputDict[item] = 1
+  OutputDict = {}
+  for item in inputVal:
+    OutputDict[item] = 1
 
-    return(OutputDict)
+  return(OutputDict)
+
 
 AmenitiesDF = json_normalize(ListingFile.amenities.apply(CleanAndSplit)).add_prefix('amenities_')
-AmenitiesDF.drop('amenities_', axis = 1, inplace= True)
-AmenitiesDF.fillna(value = 0, inplace=True)
+AmenitiesDF.drop('amenities_', axis=1, inplace=True)
+AmenitiesDF.fillna(value=0, inplace=True)
 
-HostVerificationsDF = json_normalize(ListingFile.host_verifications.apply(CleanAndSplit, alsoRemoveSingleInvComma = True)).add_prefix('host_verifications_')
-HostVerificationsDF.drop(['host_verifications_'], axis = 1, inplace = True)
-HostVerificationsDF.fillna(value = 0, inplace=True)
+HostVerificationsDF = json_normalize(ListingFile.host_verifications.apply(CleanAndSplit, alsoRemoveSingleInvComma=True)).add_prefix('host_verifications_')
+HostVerificationsDF.drop(['host_verifications_'], axis=1, inplace=True)
+HostVerificationsDF.fillna(value=0, inplace=True)
 
 ListingFile = ListingFile.join([AmenitiesDF, HostVerificationsDF])
 
@@ -56,7 +60,7 @@ removeCols = ['summary',
               'host_verifications',
               'amenities']
 
-ListingFile.drop(removeCols, axis = 1, inplace = True)
+ListingFile.drop(removeCols, axis=1, inplace=True)
 
 PriceCols = [x for x in ListingFile.columns if 'price' in x]
 PriceCols.append('cleaning_fee')
@@ -64,13 +68,13 @@ PriceCols.append('security_deposit')
 PriceCols.append('extra_people')
 
 for PriceCol in PriceCols:
-    ListingFile.loc[:,PriceCol].fillna(value = 0, inplace = True)
-    ListingFile.loc[:,PriceCol] = ListingFile.loc[:,PriceCol].astype(str).str.replace('$', '')
-    ListingFile.loc[:,PriceCol] = ListingFile.loc[:,PriceCol].str.replace(',', '')
-    ListingFile.loc[:,PriceCol] = to_numeric(ListingFile.loc[:,PriceCol])
+  ListingFile.loc[:, PriceCol].fillna(value=0, inplace=True)
+  ListingFile.loc[:, PriceCol] = ListingFile.loc[:, PriceCol].astype(str).str.replace('$', '')
+  ListingFile.loc[:, PriceCol] = ListingFile.loc[:, PriceCol].str.replace(',', '')
+  ListingFile.loc[:, PriceCol] = to_numeric(ListingFile.loc[:, PriceCol])
 
 DateConversionCols = ['last_scraped', 'host_since', 'calendar_last_scraped', 'first_review', 'last_review']
 for DateCol in DateConversionCols:
-    ListingFile.loc[:,DateCol] = to_datetime(ListingFile.loc[:,DateCol], format= '%Y-%m-%d')
+  ListingFile.loc[:, DateCol] = to_datetime(ListingFile.loc[:, DateCol], format='%Y-%m-%d')
 
-ListingFile.to_csv('~/Documents/CleanListings.csv', index = False)
+ListingFile.to_csv('~/Documents/CleanListings.csv', index=False)
